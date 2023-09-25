02a093a31ae25e754f0ee4515e8eb9315ec59e3d
clare-811 <143788190+clare-811@users.noreply.github.com>
2023-09-25 17:57:10 +0900

    Add files via upload

--- lect4_2.py
+++ lect4_2.py
@@ -88,10 +88,74 @@ print(fres)
 nvres = rd.normalvariate()
 print(nvres) """
 
-import mod.utils as mu
+""" import mod.utils as mu
 
 my_list = ['apple', 'banana', 'cherry']
 print(mu.rd_int(1,100))
 print(mu.rd_list(my_list))
 print(mu.rd_rd())
-print(mu.rd_nmvar())
+print(mu.rd_nmvar()) """
+
+""" from datetime import datetime as dt
+
+print(dt.now())
+
+#문자열을 날짜로 변환
+date_string = '2021-07-08'
+date_object = dt.strptime(date_string, '%Y-%m-%d')
+print(date_object)
+
+#날짜를 문자열로 변환
+date_object = dt.now()
+date_string = date_object.strftime('%Y-%m-%d')
+print(date_string)
+ """
+""" 
+import mod.utils as mu
+
+dtnow = mu.get_dtnow()
+print(dtnow)
+
+ret = mu.cvt_time2str("2023-09-25")
+print(ret)
+
+res = mu.cvt_str2time()
+print(res) """
+
+""" import os
+
+# 현재 작업 디렉토리 출력
+print(os.getcwd())
+
+#디렉토리 변경
+os.chdir('../')
+
+#변경된 디렉토리 출력
+print(os.getcwd())
+
+#파일 목록 출력
+print(os.listdir())
+
+#디렉토리 생성
+os.mkdir('new_directory')
+print(os.listdir())
+
+#디렉토리 삭제
+os.rmdir('mew_directory')
+print(os.listdir()) """
+
+""" import mod.utils as mu
+import os
+
+print(mu.get_curdir())
+
+pname = "python"
+mu.os_mkdir(pname)
+print(os.listdir())
+
+os.rmdir("python")
+print(os.listdir()) """
+
+import sys
+print(sys.version)
+print(sys.argv)
